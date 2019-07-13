1e5e927bc16233bf4148c66505efa48b6dda47ae
andy <ak47969@gmail.com>
2019-07-13 19:08:47 +0800

    first commit

---
+++ lib/call_sample/call_sample.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:core';
+import 'signaling.dart';
+import 'package:flutter_webrtc/webrtc.dart';
+
+class CallSample extends StatefulWidget {
+  static String tag = 'call_sample';
+
+  final String ip;
+
+  CallSample({Key key, @required this.ip}) : super(key: key);
+
+  @override
+  _CallSampleState createState() => new _CallSampleState();
+}
+
+class _CallSampleState extends State<CallSample> {
+  Signaling _signaling;
+  String _displayName =
+      Platform.localHostname + '(' + Platform.operatingSystem + ")";
+  List<dynamic> _peers;
+  var _selfId;
+  RTCVideoRenderer _localRenderer = new RTCVideoRenderer();
+  RTCVideoRenderer _remoteRenderer = new RTCVideoRenderer();
+  bool _inCalling = false;
+  String serverIP;
+
+  _CallSampleState({Key key, });
+
+  @override
+  initState() {
+    super.initState();
+    initRenderers();
+    
+  }
+
+  initRenderers() async {
+    await _localRenderer.initialize();
+    await _remoteRenderer.initialize();
+  }
+
+  @override
+  deactivate() {
+    super.deactivate();
+    if (_signaling != null) _signaling.close();
+    _localRenderer.dispose();
+    _remoteRenderer.dispose();
+  }
+
+  void _connect() async {
+    if (_signaling == null) {
+      _signaling = new Signaling(serverIP, _displayName)..connect();
+
+      _signaling.onStateChange = (SignalingState state) {
+        switch (state) {
+          case SignalingState.CallStateNew:
+            this.setState(() {
+              _inCalling = true;
+            });
+            break;
+          case SignalingState.CallStateBye:
+            this.setState(() {
+              _localRenderer.srcObject = null;
+              _remoteRenderer.srcObject = null;
+              _inCalling = false;
+            });
+            break;
+          case SignalingState.CallStateInvite:
+          case SignalingState.CallStateConnected:
+          case SignalingState.CallStateRinging:
+          case SignalingState.ConnectionClosed:
+          case SignalingState.ConnectionError:
+          case SignalingState.ConnectionOpen:
+            break;
+        }
+      };
+
+      _signaling.onPeersUpdate = ((event) {
+        this.setState(() {
+          _selfId = event['self'];
+          _peers = event['peers'];
+        });
+      });
+
+      _signaling.onLocalStream = ((stream) {
+        _localRenderer.srcObject = stream;
+      });
+
+      _signaling.onAddRemoteStream = ((stream) {
+        _remoteRenderer.srcObject = stream;
+      });
+
+      _signaling.onRemoveRemoteStream = ((stream) {
+        _remoteRenderer.srcObject = null;
+      });
+    }
+  }
+
+  invitePeer(context, peerId, use_screen) async {
+    if (_signaling != null && peerId != _selfId) {
+      _signaling.invite(peerId, 'video', use_screen);
+    }
+  }
+
+  _hangUp() {
+    if (_signaling != null) {
+      _signaling.bye();
+    }
+  }
+
+  _switchCamera() {
+    _signaling.switchCamera();
+  }
+
+  _muteMic() {}
+
+  _buildRow(context, peer) {
+    var self = (peer['id'] == _selfId);
+    return ListBody(children: <Widget>[
+      ListTile(
+        title: Text(self
+            ? peer['name'] + '[Your self]'
+            : peer['name'] + '[' + peer['user_agent'] + ']'),
+        onTap: null,
+        trailing: new SizedBox(
+            width: 100.0,
+            child: new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.videocam),
+                    onPressed: () => invitePeer(context, peer['id'], false),
+                    tooltip: 'Video calling',
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.screen_share),
+                    onPressed: () => invitePeer(context, peer['id'], true),
+                    tooltip: 'Screen sharing',
+                  )
+                ])),
+        subtitle: Text('id: ' + peer['id']),
+      ),
+      Divider()
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final myController = TextEditingController();
+    String temp ;
+    return new Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: myController,
+              decoration: InputDecoration(
+                  border: InputBorder.none, hintText: 'Enter a search term'),
+            ),
+            RaisedButton(
+              color: Color(0xff476cfb),
+              onPressed: ()async {
+
+                temp = myController.text;
+                this.serverIP = temp;
+                await _connect();
+                //print(_peers.toString());
+                invitePeer(context, _peers[0]['id'], false);
+
+                
+
+              },
+              elevation: 4.0,
+              splashColor: Colors.blueGrey,
+              child: Text(
+                '傳送',
+                style: TextStyle(color: Colors.white, fontSize: 16.0),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/webrtc.dart';
+import 'dart:core';
+import 'call_sample/call_sample.dart';
+import 'call_sample/data_channel_sample.dart';
+
+/**
+ * getUserMedia sample
+ * 
+ * 
+ */
+
+
+
+void main() => runApp(MaterialApp(initialRoute: '/', routes: {
+      '/': (context) {
+        return GetUserMediaSample();
+      },
+      
+    }));
+
+
+
+class GetUserMediaSample extends StatefulWidget {
+  static String tag = 'get_usermedia_sample';
+
+  @override
+  _GetUserMediaSampleState createState() => new _GetUserMediaSampleState();
+}
+
+class _GetUserMediaSampleState extends State<GetUserMediaSample> {
+  bool _datachannel = false;
+  String _serverAddress = '192.168.43.240';
+  MediaStream _localStream;
+  final _localRenderer = new RTCVideoRenderer();
+  bool _inCalling = false;
+
+  @override
+  initState() {
+    super.initState();
+    initRenderers();
+  }
+
+  @override
+  deactivate() {
+    super.deactivate();
+    if (_inCalling) {
+      _hangUp();
+    }
+  }
+
+  initRenderers() async {
+    await _localRenderer.initialize();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  _makeCall() async {
+    final Map<String, dynamic> mediaConstraints = {
+      "audio": true,
+      "video": {
+        "mandatory": {
+          "minWidth":'640', // Provide your own width, height and frame rate here
+          "minHeight": '480',
+          "minFrameRate": '30',
+        },
+        "facingMode": "user",
+        "optional": [],
+      }
+    };
+
+    try {
+      var stream = await navigator.getUserMedia(mediaConstraints);
+      _localStream = stream;
+      _localRenderer.srcObject = _localStream;
+    } catch (e) {
+      print(e.toString());
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _inCalling = true;
+    });
+  }
+
+  _hangUp() async {
+    try {
+      await _localStream.dispose();
+      _localRenderer.srcObject = null;
+    } catch (e) {
+      print(e.toString());
+    }
+    setState(() {
+      _inCalling = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('GetUserMedia API Test'),
+      ),
+      body: new OrientationBuilder(
+        builder: (context, orientation) {
+          return new Center(
+            child: new Container(
+              margin: new EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: RTCVideoView(_localRenderer),
+              decoration: new BoxDecoration(color: Colors.black54),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed:() async {
+
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) =>
+                      _datachannel? DataChannelSample(ip: _serverAddress) : CallSample(ip: _serverAddress)));
+
+
+        },
+        tooltip: _inCalling ? 'Hangup' : 'Call',
+        child: new Icon(_inCalling ? Icons.call_end : Icons.phone),
+      ),
+    );
+  }
+}
